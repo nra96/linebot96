b241679ced43388010bbe5dfc734464a9db58dfd
Noor Clean <noorclean98@gmail.com>
2020-12-05 15:36:38 +0700

    initial commit

---
+++ src/test/java/com/dicoding/implementasiFiturJava/ImplementasiFiturJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicoding.implementasiFiturJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementasiFiturJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
